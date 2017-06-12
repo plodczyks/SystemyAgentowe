6ebb46e5a5487c27f6140e0fb683eea2305a18e3
plodczyks <slawekplodczyk@onet.eu>
2017-06-12 21:57:19 +0200

    Merge branch 'master' of https://github.com/plodczyks/SystemyAgentowe

--- MiniServer/src/Main.java
+++ MiniServer/src/Main.java
@@ -1,11 +1,16 @@
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 /**
  * Created by asd on 24.05.2017.
  */
 public class Main {
-    public static void main(String[] args) throws InterruptedException, URISyntaxException {
+    public static void main(String[] args) throws InterruptedException, URISyntaxException, IOException {
         WebSServer server = new WebSServer();
         server.start();
+        File htmlFile = new File("js/simulation.html");
+        Desktop.getDesktop().browse(htmlFile.toURI());
     }
 }
